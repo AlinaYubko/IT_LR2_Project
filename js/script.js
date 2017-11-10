3d60ac1fc4e90e0d807e6d528f8151a1f8de1927
Ильюшенок Илья <32908895+ilyushenok@users.noreply.github.com>
2017-11-10 11:09:07 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+function setup(){
+	var settings = document.getElementById("settings");
+	settings.className = 'settings-normal';
+	
+	var collapse = document.getElementById("collapse");
+	collapse.innerHTML = '<span>Свернуть</span>';
+	collapse.onclick = function(){
+		var settings = document.getElementById("settings");
+		console.log(settings.className);
+		if(settings.className == '' || settings.className == 'settings-normal'){
+			settings.className = 'settings-collapsed';
+			this.innerHTML = '<span>Развернуть</span>';
+		}else{
+			settings.className = 'settings-normal';
+			this.innerHTML = '<span>Свернуть</span>';
+		}
+	}
+}
 
+window.onload = function(){
+	setup();
+}
