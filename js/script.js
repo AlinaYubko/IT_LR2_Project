07bebd1a7f2b71fa34dae79a571950d87df60ab5
Ильюшенок Илья <32908895+ilyushenok@users.noreply.github.com>
2017-11-10 13:26:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// board size
+var boardWidth = 100;
+var boardHeight = 100;
+
+// board move
+var boardMove = false;
+var boardMX, boardMY;
+var boardMSkip = false;
+
+// board cell
+var cellSize = 20;
+var cellSizeMin = 5;
+var cellSizeMax = 50;
+var cellSizeDelta = 2;
+
 function setup(){
 	var settings = document.getElementById("settings");
 	settings.className = 'settings-normal';
@@ -32,6 +47,86 @@ function setup(){
 	remSet.onclick = function(){
 		remLastSet();
 	}
+	
+	var board = document.getElementById('board');
+	var table = document.createElement('table');
+	for(var j = 0; j < boardHeight; j++){
+		var tr = document.createElement('tr');
+		
+		for(var i = 0; i < boardWidth; i++){
+			var td = document.createElement('td');
+			var cell = document.createElement('div');
+			cell.className = 'cell';
+			td.appendChild(cell);
+			
+			tr.appendChild(td);
+		}
+		
+		table.appendChild(tr);
+	}
+	
+	board.appendChild(table);
+	
+	board.onmousedown = function(event){
+		boardMove = true;
+		boardMX = event.clientX;
+		boardMY = event.clientY;
+	}
+	
+	board.onmouseup = function(){
+		boardMove = false;
+	}
+	window.onmouseup = function(){
+		boardMove = false;
+	}
+	
+	window.onmousemove = function(event){
+		if(boardMove){
+			var nbMX = event.clientX;
+			var nbMY = event.clientY;
+			window.scrollBy(boardMX - nbMX, boardMY - nbMY);
+			boardMX = nbMX;
+			boardMY = nbMY;
+		}
+	}
+	
+	window.addEventListener("mousewheel", MouseWheelHandler, false);
+	window.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+}
+
+function MouseWheelHandler(e) {
+	e.preventDefault();
+	
+	// cross-browser wheel delta
+	var e = window.event || e; // old IE support
+	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+	
+	console.log("delta " + delta);
+	if(delta > 0 || delta < 0){
+		if(delta > 0){
+			cellSize += cellSizeDelta;
+		}else if(delta < 0){
+			cellSize -= cellSizeDelta;
+		}
+		
+		if(cellSize < cellSizeMin){
+			cellSize = cellSizeMin;
+		}
+		
+		if(cellSize > cellSizeMax){
+			cellSize = cellSizeMax;
+		}
+		
+		jss.set('div.cell', {'width': cellSize+'px', 'height': cellSize+'px'});
+		/*
+		var el = document.getElementsByClassName("cell");
+		for(var i = 0; i < el.length; i++){
+			el[i].style.width = cellSize + "px";
+			el[i].style.height = cellSize + "px";
+		}*/
+	}
+	
+	return false;
 }
 
 function addNewSet(){
